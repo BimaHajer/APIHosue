d7ec21a47570fa645369dd0b461212ca505d8377
unknown <bima.tn.dev.h.f@gmail.com>
2025-01-16 10:16:04 +0100

    userlogin

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,13 +1,27 @@
 import { Injectable } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
-
+import { User } from './entities/user.entity';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import * as bcrypt from 'bcrypt';
 @Injectable()
 export class UserService {
-  create(createUserDto: CreateUserDto) {
-    return 'This action adds a new user';
-  }
+  constructor(
+    @InjectRepository(User)
+    private readonly userRepository:Repository<User>
+    ){}
 
+  create(createUserDto: CreateUserDto) {
+  let user=this.userRepository.create(createUserDto)
+  user.password=(this.hashPassword(user.password)).toString()
+
+  }
+  async hashPassword(password:string){
+const saltOrRounds = 29;
+const hash = await bcrypt.hash(password, saltOrRounds);
+return hash
+}
   findAll() {
     return `This action returns all user`;
   }
@@ -24,3 +38,5 @@ export class UserService {
     return `This action removes a #${id} user`;
   }
 }
+
+
