070a50d43b8ef8c8f00b823f7117b682aafaed00
moufidaSanai <moufidasanai47@gmail.com>
2025-02-03 11:07:46 +0100

    house

--- src/equipement/entities/equipement.entity.ts
+++ src/equipement/entities/equipement.entity.ts
@@ -5,8 +5,10 @@ import { BeforeInsert, BeforeRemove, BeforeUpdate, Column, Entity, PrimaryGenera
 export class Equipement {
      @PrimaryGeneratedColumn()
         id: number;
-        @Column("text",{name:"name",nullable:true})
-        name: string;
+        @Column("text",{name:"title",nullable:true})
+        title: string;
+        @Column("text",{name:"description",nullable:true})
+        description: string;
         @Column("text",{name:"condition",nullable:true})
         condition: string;
         @Column("text",{name:"quantity",nullable:true})
@@ -14,12 +16,20 @@ export class Equipement {
         @Column("date",{name:"createdAt",nullable:true})
         created_at: Date;
         @Column("date",{name:"createdBy",nullable:true})
-        created_by: Date;
+        created_by: number;
         @Column("date",{name:"updateAt",nullable:true})
         updated_at: Date;
         @Column("date",{name:"updateBy",nullable:true})
-        updated_by: Date;        
+        updated_by: number; 
+        @Column("date",{name:"deleted_at",nullable:true})       
         deleted_at: Date;
+        @Column("date",{name:"deleted_by",nullable:true})       
+        deleted_by: number;
+        @Column("date",{name:"isavaible",nullable:true})
+        isavaible: boolean;
+        @Column("date",{name:"active",nullable:true})
+        active: boolean;
+        
     
     
     
---
+++ src/house/house.service.ts
@@ -0,0 +1,42 @@
+import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { CreateHouseDto } from './dto/create-house.dto';
+import { UpdateHouseDto } from './dto/update-house.dto';
+import { House } from './entities/house.entity';
+import { Repository } from 'typeorm';
+
+@Injectable()
+export class HouseService {
+   constructor(
+      @InjectRepository(House)
+      private readonly houseRepository: Repository<House>,
+    ) {}
+  
+    async create(houseData: CreateHouseDto) {
+      let house =  await this.houseRepository.create(houseData);
+      console.log(house)
+      return this.houseRepository.save(house)  }
+  
+    async findAll() {
+      return  this.houseRepository.findAndCount();
+    }
+  
+    async findOne(id: number){
+      return await this.houseRepository.findOne({ where: { id } });
+    }
+  
+    async update(id: number, updateHouseDto:UpdateHouseDto) {
+      let house = await this.houseRepository.preload({
+        id:+id,
+        ...UpdateHouseDto
+      })
+     return this.houseRepository.save(house)
+    }
+    delete(id: number) {
+      this.houseRepository.delete(id)
+    }
+      
+   
+    
+  }
+  
