205a97675c21950ecb71d26f7bece056e226d0ba
moufidaSanai <moufidasanai47@gmail.com>
2025-02-27 09:40:10 +0100

    caracteristiquemodul

--- src/characteristic/entities/characteristic.entity.ts
+++ src/characteristic/entities/characteristic.entity.ts
@@ -1,35 +1,60 @@
-import { BeforeInsert,  BeforeUpdate, Column, Entity, PrimaryGeneratedColumn } from "typeorm";
-@Entity("Characteristic")
+import { Column, Entity, PrimaryGeneratedColumn, BeforeInsert, BeforeUpdate } from 'typeorm';
 
+@Entity('characteristic')
 export class Characteristic {
-    @PrimaryGeneratedColumn()
-    id: number;
-    @Column("text",{name:"Key",nullable:true})
-    Key: string;
-    @Column("text",{name:"title",nullable:true})
-    title:string;
-    @Column("text",{name:"description",nullable:true})
-    description:string
-    @Column("text",{name:"isvaible",nullable:true})
-    isvaible:boolean
-    @Column("text",{name:"isdelete",nullable:true})
-    isdelete:boolean
-    @Column("text",{name:"deleteAT",nullable:true})
-    deleteAT: Date;
-    @Column("text",{name:"value",nullable:true})
-    value: string;
-    @Column("date",{name:"createAt",nullable:true})
-    created_at: Date;
-    @Column("date",{name:"updateAt",nullable:true})
-    updated_at: Date;
-   
-    @BeforeInsert()
-    DateCreateAT(){
-        this.created_at= new Date()// date de systeme
-    }
-    @BeforeUpdate()
-    DateUpdateAT(){
-        this.updated_at= new Date()
-    }
-    
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column('text', { name: 'title', nullable: true })
+  title: string;
+
+  @Column('text', { name: 'description', nullable: true })
+  description: string;
+  @Column('text', { name: 'image', nullable: true })
+   image: string;
+
+  @Column('text', { name: 'nom', nullable: true })
+  nom: string;
+  
+ 
+  @Column('text', { name: 'condition', nullable: true })
+  condition: string;
+
+  @Column('int', { name: 'quantity', nullable: true })
+  quantity: number;
+
+  @Column('boolean', { name: 'isavaible', nullable: true })
+  isavaible: boolean;
+
+  @Column('boolean', { name: 'active', nullable: true })
+  active: boolean;
+
+  @Column('date', { name: 'createdAt', nullable: true })
+  created_at: Date;
+
+  @Column('int', { name: 'createdBy', nullable: true })
+  created_by: number;
+
+  @Column('date', { name: 'updatedAt', nullable: true })
+  updated_at: Date;
+
+  @Column('int', { name: 'updatedBy', nullable: true })
+  updated_by: number;
+
+  @Column('date', { name: 'deleted_at', nullable: true })
+  deleted_at: Date;
+
+  @Column('int', { name: 'deleted_by', nullable: true })
+  deleted_by: number;
+
+  @BeforeInsert()
+  setCreateDate() {
+    this.created_at = new Date();
+    this.updated_at = new Date();
+  }
+
+  @BeforeUpdate()
+  setUpdateDate() {
+    this.updated_at = new Date();
+  }
 }
